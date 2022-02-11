f676ae82cac3136b8eefff9040cf4e1a229b3040
SudeshnaMukherjee94 <91111960+SudeshnaMukherjee94@users.noreply.github.com>
2022-02-11 11:37:06 +0530

    Create ColorPairFromPairNumber.c

---
+++ ColorPairFromPairNumber.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "ColorsDefinition.c"
+#include "FormColorPair.c"
+
+//GET COLOR PAIR FROM PAIR NUMBER
+
+ColorPair GetColorFromPairNumber(int pairNumber) {
+    ColorPair colorPair;
+    int zeroBasedPairNumber = pairNumber - 1;
+    colorPair.majorColor = 
+        (enum MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+    colorPair.minorColor =
+        (enum MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+    return colorPair;
+}
