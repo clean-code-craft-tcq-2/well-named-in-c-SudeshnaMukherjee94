df6e425c0ca4ac4c5f4734f67f1b759fe2f4f52a
SudeshnaMukherjee94 <91111960+SudeshnaMukherjee94@users.noreply.github.com>
2022-02-11 11:52:52 +0530

    Delete PairNumberFromColorPair.c

--- PairNumberFromColorPair.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "ColorsDefinition.c"
-
-//GET PAIR NUMBER FROM COLOR PAIR
-
-int GetPairNumberFromColor(const ColorPair* colorPair) {
-    return colorPair->majorColor * numberOfMinorColors +
-            colorPair->minorColor + 1;
-}
