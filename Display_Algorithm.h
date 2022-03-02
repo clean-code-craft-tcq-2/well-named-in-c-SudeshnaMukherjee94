d69234aa08b758ff239d9e081b21ce00803f2606
SudeshnaMukherjee94 <91111960+SudeshnaMukherjee94@users.noreply.github.com>
2022-03-03 00:53:41 +0530

    Update and rename TestSpec.c to Display_Algorithm.h

---
+++ Display_Algorithm.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <assert.h>
+#include "Color_Selection_Algorithm.h"
+
+void testNumberToPair( int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
+void testPairToNumber( enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
