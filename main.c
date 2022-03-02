b03795ee0276bb71aa3742f61e03cd8917cde4cc
SudeshnaMukherjee94 <91111960+SudeshnaMukherjee94@users.noreply.github.com>
2022-03-03 00:52:39 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-#include <assert.h>
-#include "TestSpec.c"
-    
+#include "Display_Algorithm.h"
+
 int main() {
     testNumberToPair(4, WHITE, BROWN);
     testNumberToPair(5, WHITE, SLATE);
 
     testPairToNumber(BLACK, ORANGE, 12);
     testPairToNumber(VIOLET, SLATE, 25);
-
+    
     return 0;
 }
